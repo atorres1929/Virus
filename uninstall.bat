4089a894fc169ee278e9c495b13c7f0d86c535e1
Adam Torres <atorres1929@gmail.com>
2016-10-28 01:26:39 -0400

    Corrected uninstall.bat

--- uninstall.bat
+++ uninstall.bat
@@ -1,6 +1,6 @@
 cd C:
 RD /S /Q "%CD%\Virus"
-del "C:\Users\Adam\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\virus.bat"
+del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\virus.bat"
 RD /S /Q "%APPDATA%\Not a Virus"
 echo =======================================================
 echo You should now be clear of the Virus
